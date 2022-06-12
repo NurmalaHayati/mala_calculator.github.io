354a344148d64e831b35992b5c94c30f5826dd33
Nurmala Hayati <nurmala.hayati32@gmail.com>
2022-06-12 20:55:54 +0700

    perubahan

--- script.js
+++ script.js
@@ -1,32 +1,95 @@
-const calculatorScreen = document.querySelector(".calculator-screen")
-
-const updateScreen = (number)=>{
-    calculatorScreen.value = number
-}
 
 const numbers = document.querySelectorAll(".number")
-
-numbers.forEach((number) => {
-    number.addEventListener("click",(event) =>{
-      updateScreen("event.target.value")
-    })
-})
-
-let prevNumber=''
-let calculatorOperator=''
-let currentNumber='0'
-
-const inputNumber = (number) => {
-    if (currentNumber === '0'){
-        currentNumber = number
-    } else {
-     currentNumber += number
-    }
-}
-numbers.forEach((number) => {
-    number.addEventListener("click", (event)=>{
-        inputNumber = (event.target.value)
+numbers.forEach((number)=> {
+    number.addEventListener("click",(event)=>{
+        inputNumber(event.target.value)
         updateScreen(currentNumber)
     })
 })
+
+const calculatorScreen = document.querySelector('.calculator-screen')
+
+const updateScreen = (number) => {
+    calculatorScreen.value = number
+}
+
+let prevNumber = ''
+let calculationOperator = 'calculate'
+let currentNumber ='0'
+
+const inputNumber = (number) => {
+    if(currentNumber==='0'){
+        currentNumber = number
+    }else {
+    currentNumber += number
+    }
+}
+
+// number.forEach((number) => {
+//     number.addEventListener("click", (event) => {
+//         inputNumber(event.target.value)
+//         updateScreen(currentNumber)
+//     })
+// })
+
+const operators = document.querySelectorAll(".operator")
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+        inputOperator(event.target.value)
+    })
+})
+
+const inputOperator =(operator) => {
+    prevNumber = currentNumber
+    calculationOperator = operator
+    currentNumber =''
+}
+
+const equalSign = document.querySelector('.equal-sign')
+
+equalSign.addEventListener('click', () => {
+    calculate()
+    updateScreen(currentNumber)
+})
+
+const calculate =()=>{
+    let result = ''
+    switch(calculationOperator){
+        case "+":
+            result = prevNumber + prevNumber
+            break
+        case "-":
+            result = prevNumber - prevNumber
+            break
+        case '*':
+            result = prevNumber * prevNumber
+            break
+        case '/':
+            result = prevNumber / prevNumber
+            break
+        default:
+            break
+    }
+    currentNumber = result
+    calculationOperator =''
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
